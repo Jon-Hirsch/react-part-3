5af93e35e37b159295d1608c11742c9ab32a27c9
Jon Hirsch <jonmh99@gmail.com>
2022-05-16 15:48:45 -0700

    update components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import {Component} from 'react';
-import HelloWorld from './HelloWorld';
-import NavBar from './NavBar';
+import React from "react";
+import HelloWorld from "./HelloWorld";
+import NavBar from "./NavBar";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <HelloWorld />
-        <HelloWorld />
-        <HelloWorld />
-        <NavBar />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <NavBar />
+    </div>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { Component } from "react";
 import "./css/navbar.css";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <div className="navbar">
-        <a className="navbar--link" href="/">
-          Home
-        </a>
-        <a className="navbar--link" href="/personal-projects">
-          Personal Projects
-        </a>
-        <a className="navbar--link" href="/articles">
-          Articles
-        </a>
-        <a className="navbar--link" href="/search">
-          Search
-        </a>
-      </div>
-    );
-  }
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <a className="navbar--link" href="/">
+        Home
+      </a>
+      <a className="navbar--link" href="/personal-projects">
+        Personal Projects
+      </a>
+      <a className="navbar--link" href="/articles">
+        Articles
+      </a>
+      <a className="navbar--link" href="/search">
+        Search
+      </a>
+    </div>
+  );
 }
