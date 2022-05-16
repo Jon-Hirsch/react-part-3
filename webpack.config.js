05053fd57040166d3fa1837b3eb7e967ba8988fa
Jon Hirsch <JonMH99@gmail.com>
2022-05-16 15:37:26 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,30 +5,6 @@ module.exports = {
     filename: "bundle.js",
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel",
-        exclude: /node_modules/,
-        query: {
-          presets: ["es2015", "react"],
-        },
-      },
-      {
-        test: /\.css$/,
-        loaders: ["style", "css"],
-      },
-    ],
-  },
-};
-
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-  },
-  module: {
     rules: [
       {
         test: /\.jsx?$/,
